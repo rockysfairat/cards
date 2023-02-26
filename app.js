d553f1bc41a49ac0f384eb3a7d648722888b588e
Ålexander Yurchenko <rockysfairat@asus-x512.lan>
2023-02-26 22:28:54 +0100

    Added three animations in app.js

--- app.js
+++ app.js
@@ -9,12 +9,7 @@ const data = [
     charisma: 30,
     img: "jack.jpeg",
     alt: "Jack the lizard guy",
-    text: [
-      "Jack is a unique creature, unlike any other in the world. He is half human and half lizard, giving him a truly fascinating appearance. His green, scaly skin is both intimidating and intriguing, with a texture that is both rough and smooth to the touch. His long, forked tongue flickers in and out of his mouth, giving him a reptilian air that is both eerie and captivating.",
-      "Despite his intimidating appearance, however, Jack is actually quite friendly and enjoys spending time lounging in the sun, just like any other lizard. He is content to spend hours basking in the warmth, soaking up the rays and feeling the heat on his skin. He loves to feel the sun on his face and the wind in his scales, and is always up for a good adventure. When he's not basking in the sun, Jack can be found exploring the wilderness, using his lizard-like abilities to climb trees and scale walls.",
-      "He is a skilled hunter, with sharp teeth and keen senses that allow him to track prey with ease. But despite his hunting skills, Jack is a gentle soul at heart, and enjoys spending time with other creatures, including humans.",
-      "He is always eager to make new friends and to explore new places, and his unusual appearance only makes him more fascinating to those who meet him.",
-    ],
+    text: "<p>Jack is a unique creature, unlike any other in the world. He is half human and half lizard, giving him a truly fascinating appearance. His green, scaly skin is both intimidating and intriguing, with a texture that is both rough and smooth to the touch. His long, forked tongue flickers in and out of his mouth, giving him a reptilian air that is both eerie and captivating.</p> <p>Despite his intimidating appearance, however, Jack is actually quite friendly and enjoys spending time lounging in the sun, just like any other lizard. He is content to spend hours basking in the warmth, soaking up the rays and feeling the heat on his skin. He loves to feel the sun on his face and the wind in his scales, and is always up for a good adventure. When he's not basking in the sun, Jack can be found exploring the wilderness, using his lizard-like abilities to climb trees and scale walls.</p> <p>He is a skilled hunter, with sharp teeth and keen senses that allow him to track prey with ease. But despite his hunting skills, Jack is a gentle soul at heart, and enjoys spending time with other creatures, including humans.</p> <p>He is always eager to make new friends and to explore new places, and his unusual appearance only makes him more fascinating to those who meet him.</p>",
   },
   {
     id: "Nikolas",
@@ -26,12 +21,7 @@ const data = [
     charisma: 80,
     img: "Nikolas.jpeg",
     alt: "Nikolas the tigerman",
-    text: [
-      "Nikolas is a truly remarkable creature, unlike any other in the world.",
-      "He is half human and half tiger, giving him a fierce and powerful presence that is both intimidating and awe-inspiring. His golden fur is soft and silky to the touch, with black stripes that stand out against his coat. His piercing green eyes seem to glow in the dark, giving him an otherworldly air that is both beautiful and terrifying. He has razor-sharp claws and teeth, making him a formidable opponent in any situation.",
-      "Despite his wild nature, however, Nikolas has a strong sense of loyalty and will fiercely protect those he cares about. He is a fierce fighter, with lightning-fast reflexes and a strength that belies his hybrid nature. He is both graceful and powerful, moving with the fluidity of a big cat and the strength of a human.",
-      "He is a creature of great courage and determination, never backing down from a challenge and always willing to put himself in harm's way to protect others. He is a force to be reckoned with, and those who underestimate him do so at their own peril.",
-    ],
+    text: "<p>Nikolas is a truly remarkable creature, unlike any other in the world. <p>He is half human and half tiger, giving him a fierce and powerful presence that is both intimidating and awe-inspiring. His golden fur is soft and silky to the touch, with black stripes that stand out against his coat. His piercing green eyes seem to glow in the dark, giving him an otherworldly air that is both beautiful and terrifying. He has razor-sharp claws and teeth, making him a formidable opponent in any situation.</p> <p>Despite his wild nature, however, Nikolas has a strong sense of loyalty and will fiercely protect those he cares about. He is a fierce fighter, with lightning-fast reflexes and a strength that belies his hybrid nature. He is both graceful and powerful, moving with the fluidity of a big cat and the strength of a human.</p> <p>He is a creature of great courage and determination, never backing down from a challenge and always willing to put himself in harm's way to protect others. He is a force to be reckoned with, and those who underestimate him do so at their own peril.</p>",
   },
   {
     id: "Stephen",
@@ -43,22 +33,21 @@ const data = [
     charisma: 95,
     img: "Stephen.jpeg",
     alt: "Stephen the human",
-    text: [
-      "Stephen is a red-haired human with a mischievous twinkle in his eye, and an impressive muscular build. He is always up for a good time, and loves nothing more than to tell jokes and play pranks on his friends. His fiery red hair is both striking and beautiful, with a vibrant hue that sets him apart from others.",
-      "His toned physique is a testament to his dedication to physical fitness, and his impressive muscles only add to his confident and powerful presence. He has a quick wit and a sharp tongue, making him both charming and a bit of a troublemaker.",
-      "He loves to push boundaries and challenge conventions, always looking for new and interesting ways to break the mold. But despite his lighthearted nature, Stephen has a heart of gold and will go out of his way to help someone in need. He is fiercely loyal to his friends and family, and will stop at nothing to protect those he cares about.",
-      "He is a true original, unafraid to be himself and to stand out from the crowd. He is a breath of fresh air in a world that can sometimes feel dull and lifeless, and his infectious spirit is contagious to all those who know him.",
-    ],
+    text: "<p>Stephen is a red-haired human with a mischievous twinkle in his eye, and an impressive muscular build. He is always up for a good time, and loves nothing more than to tell jokes and play pranks on his friends. His fiery red hair is both striking and beautiful, with a vibrant hue that sets him apart from others.</p> <p>His toned physique is a testament to his dedication to physical fitness, and his impressive muscles only add to his confident and powerful presence. He has a quick wit and a sharp tongue, making him both charming and a bit of a troublemaker.</p> <p>He loves to push boundaries and challenge conventions, always looking for new and interesting ways to break the mold. But despite his lighthearted nature, Stephen has a heart of gold and will go out of his way to help someone in need. He is fiercely loyal to his friends and family, and will stop at nothing to protect those he cares about.</p> <p>He is a true original, unafraid to be himself and to stand out from the crowd. He is a breath of fresh air in a world that can sometimes feel dull and lifeless, and his infectious spirit is contagious to all those who know him.</p>",
   },
 ];
 
 // The card wrapper:
 
 const card = document.getElementsByTagName("article");
+const cardDecorationOutline = card[0].children[0];
 
 // Image block variables:
 
+// Character img + decorative <span>:
 const image = document.getElementsByTagName("img");
+const clocky = document.getElementsByClassName("clocky");
+// Character stats:
 const strengthStat = document.getElementsByClassName("strength");
 const intellectStat = document.getElementsByClassName("intellect");
 const staminaStat = document.getElementsByClassName("stamina");
@@ -88,9 +77,7 @@ function filterOb(array) {
         charismaStat[0].textContent = "Charisma: " + array[i].charisma;
         // Text block:
         charName[0].textContent = array[i].name;
-        for (let k = 0; k < array[i].text.length; k++) {
-          charDesc[0].children[k].textContent = array[i].text[k];
-        }
+        /* charDesc[0].innerHTML = array[i].text; */
       }
     }
   };
@@ -114,8 +101,19 @@ function selectBtn(event) {
     for (let i = 0; i < btn.length; i++) {
       if (btn[i].id == button.id) {
         btnId = button.id;
-        console.log(btnId);
+        // filter characters:
         filterChar(btnId);
+        // animate characters' img on every iteration:
+        // Init val (animation step 1):
+        clocky[0].style =
+          "background-image: repeating-conic-gradient(#000 0deg, #000 360deg);";
+        cardDecorationOutline.style = "background-color: none";
+        // Step 2:
+        animateCharImg();
+
+        animateCardDeco();
+
+        animateCharDescription(btnId, data);
       }
     }
   }
@@ -123,26 +121,54 @@ function selectBtn(event) {
 
 // ANIMATIONS:
 
-// Animation Img function:
-
-function animateImg() {
-  const clocky = document.getElementsByClassName("clocky");
+function animateCharImg() {
   let translucentVal = 1;
   let blackVal = 360;
-
-  function updatebg() {
+  function updateClocky() {
     translucentVal = (translucentVal + 1) % 360;
     blackVal = (blackVal - 1) % 360;
     clocky[0].style = `background-image: repeating-conic-gradient(rgba(0, 0, 0, 0) 0deg, rgba(0, 0, 0, 0) ${translucentVal}deg, #000 1deg, #000 ${blackVal}deg);`;
   }
 
-  const intervalId = setInterval(updatebg, 5);
+  const intervalIdClocky = setInterval(updateClocky, 5);
 
   setTimeout(() => {
-    clearInterval(intervalId);
+    clearInterval(intervalIdClocky);
   }, 1300);
+
+  return setTimeout(updateClocky, 2000);
 }
 
-setTimeout(() => {
-  animateImg();
-}, 2000);
+function animateCardDeco() {
+  let darkVal = 0;
+  function updateCardDeco() {
+    darkVal = darkVal + 1;
+    cardDecorationOutline.style = `background-image:  linear-gradient(-150deg, rgba(0,0,0,1) ${darkVal}%, rgba(0,0,0,0) ${darkVal}% 100%);`;
+  }
+
+  const intervalIdcardDeco = setInterval(updateCardDeco, 10);
+
+  setTimeout(() => {
+    clearInterval(intervalIdcardDeco);
+  }, 1300);
+
+  return setTimeout(updateCardDeco, 1000);
+}
+
+function animateCharDescription(name, array) {
+  console.log(array);
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].name == name) {
+      let f = 0;
+      let txt = array[i].text;
+      function typeWriter() {
+        if (f < txt.length) {
+          charDesc[0].innerHTML += txt.charAt(f);
+          f++;
+          setTimeout(typeWriter, 20);
+        }
+      }
+      typeWriter();
+    }
+  }
+}
